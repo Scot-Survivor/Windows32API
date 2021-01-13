4c3f7feebc3d892c2b8f2bf8db3bf13e08c48233
Scot_Survivor <unconfigured@null.spigotmc.org>
2021-01-13 19:42:26 +0000

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/Window32API.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Window32API.dir/main.cpp.obj"
+  "Window32API.exe"
+  "Window32API.exe.manifest"
+  "Window32API.lib"
+  "Window32API.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Window32API.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
